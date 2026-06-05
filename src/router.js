b8f9a5725af3cb6c0c8c3085997676791a0b5d6b
Antonio <antoniorfigueiras@gmail.com>
2026-06-05 19:41:47 +0100

    build: fix router import paths for deployment

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from './views/HomeView.vue'
+import HomeView from './views/TempView.vue'
 import ProjectView from './views/ProjectView.vue'
 
 const router = createRouter({
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: TempView,
     },
     {
       path: '/projeto/:id',
